ad0fce262fa3e1a54ac097c46e3bd42e9c3c8af1
Sergio Alvarez-Napagao <tranchis@gmail.com>
2018-11-20 12:43:01 +0100

    Make font bigger for hyper/macos

--- playbooks/roles/macos/templates/hyper.js
+++ playbooks/roles/macos/templates/hyper.js
@@ -9,7 +9,7 @@ module.exports = {
     updateChannel: 'stable',
 
     // default font size in pixels for all tabs
-    fontSize: 14,
+    fontSize: 16,
 
     // font family with optional fallbacks
     fontFamily: '"Fira Code", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
