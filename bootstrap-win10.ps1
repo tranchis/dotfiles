0b983e3b20d38f947dbf64166669308a1ab92c5c
Sergio Alvarez-Napagao <tranchis@gmail.com>
2020-01-05 20:06:04 +0100

    Adjust for Windows 1904

--- bootstrap-win10.ps1
+++ bootstrap-win10.ps1
@@ -6,6 +6,7 @@ Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-L
 
 choco upgrade --allow-empty-checksums -y battle.net
 choco upgrade -y firefox thunderbird pycharm dropbox battle.net hyper vim steam git git-lfs firacode itunes goggalaxy slack vcxsrv nodejs.install visualstudio2017buildtools
+choco upgrade -y etcher autohotkey icloud virtualbox winrar virtualbox.extensionpack hwmonitor intelpowergadget adobereader
 choco install -y anaconda3 --params /AddToPath
 
 if(!(Test-Path ~/Ubuntu/ubuntu1804.exe)) {
