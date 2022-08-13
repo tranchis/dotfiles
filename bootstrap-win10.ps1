bfdfdad8a87aac9bcd82eafb8a2ad550a8a0b896
Sergio Alvarez-Napagao <tranchis@gmail.com>
2022-08-14 00:33:56 +0200

    Adapt to Win11 2022

--- bootstrap-win10.ps1
+++ bootstrap-win10.ps1
@@ -5,21 +5,14 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.We
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
 
 choco upgrade --allow-empty-checksums -y battle.net
-choco upgrade -y firefox thunderbird pycharm rubymine whatsapp docker-desktop dropbox battle.net hyper vim steam git git-lfs firacode-ttf itunes goggalaxy slack vcxsrv nodejs.install visualstudio2017buildtools
+choco upgrade -y firefox thunderbird pycharm rubymine intellijidea-ultimate whatsapp docker-desktop dropbox battle.net hyper vim steam git git-lfs firacode-ttf itunes goggalaxy slack vcxsrv nodejs.install visualstudio2017buildtools
 choco upgrade -y etcher autohotkey icloud virtualbox winrar virtualbox.extensionpack hwmonitor intelpowergadget adobereader cpu-z jdk8 rocketchat hwinfo intellijidea-ultimate
-choco upgrade -y vlc cmake openssl vscode libreoffice-fresh postman discord octave pyenv-win vagrant nextcloud-client lein
+choco upgrade -y vlc cmake openssl vscode libreoffice-fresh postman discord octave pyenv-win vagrant nextcloud-client lein wsl-ubuntu-2004
 choco install -y mysql --version=5.7.18
 choco install -y anaconda3 --params /AddToPath
 
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
-
-if(!(Test-Path ~/Ubuntu/ubuntu2004.appx)) {
-  Invoke-WebRequest -Uri https://aka.ms/wslubuntu2004 -OutFile ~/Ubuntu.appx -UseBasicParsing
-  Rename-Item ~/Ubuntu.appx Ubuntu.zip
-  Expand-Archive ~/Ubuntu.zip ~/Ubuntu
-  ~/Ubuntu/ubuntu2004.exe
-}
 
 if(!(Test-Path ~/tridactyl.xpi)) {
   Invoke-WebRequest -Uri https://tridactyl.cmcaine.co.uk/betas/tridactyl-latest.xpi -OutFile ~/tridactyl.xpi -UseBasicParsing
--- bootstrap-wsl.sh
+++ bootstrap-wsl.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install bzip2 libffi libxml2 libxmlsec1 openssl readline sqlite xz zlib gcc
+
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install ansible
 
-ansible-playbook -i inventory wsl.yml
+ansible-playbook -K -i inventory wsl.yml
 
