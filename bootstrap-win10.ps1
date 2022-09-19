1189abae500df8e7e7df5b4cc6e83a3245e979dc
Sergio Alvarez-Napagao <tranchis@gmail.com>
2022-09-19 09:37:45 +0200

    Update changes for WSL Win11

--- bootstrap-win10.ps1
+++ bootstrap-win10.ps1
@@ -7,7 +7,7 @@ Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-L
 choco upgrade --allow-empty-checksums -y battle.net
 choco upgrade -y firefox thunderbird pycharm rubymine intellijidea-ultimate whatsapp docker-desktop dropbox battle.net hyper vim steam git git-lfs firacode-ttf itunes goggalaxy slack vcxsrv nodejs.install visualstudio2017buildtools
 choco upgrade -y etcher autohotkey icloud virtualbox winrar virtualbox.extensionpack hwmonitor intelpowergadget adobereader cpu-z jdk8 rocketchat hwinfo intellijidea-ultimate
-choco upgrade -y vlc cmake openssl vscode libreoffice-fresh postman discord octave pyenv-win vagrant nextcloud-client lein wsl-ubuntu-2004
+choco upgrade -y vlc cmake openssl vscode libreoffice-fresh postman discord octave pyenv-win vagrant nextcloud-client lein razer-synapse-3 forticlientvpn wsl-ubuntu-2004
 choco install -y mysql --version=5.7.18
 choco install -y anaconda3 --params /AddToPath
 
