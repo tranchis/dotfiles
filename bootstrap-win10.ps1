81e011b9afdd1f26092f61cbd3a659432d31b9d5
Sergio Alvarez-Napagao <tranchis@gmail.com>
2023-02-25 17:04:41 +0100

    Some adjustments for laptop

--- bootstrap-win10.ps1
+++ bootstrap-win10.ps1
@@ -7,9 +7,19 @@ Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-L
 choco upgrade --allow-empty-checksums -y battle.net
 choco upgrade -y firefox thunderbird pycharm rubymine intellijidea-ultimate whatsapp docker-desktop dropbox battle.net hyper vim steam git git-lfs firacode-ttf itunes goggalaxy slack vcxsrv nodejs.install visualstudio2017buildtools
 choco upgrade -y etcher autohotkey icloud virtualbox winrar virtualbox.extensionpack hwmonitor intelpowergadget adobereader cpu-z jdk8 rocketchat hwinfo intellijidea-ultimate
-choco upgrade -y vlc cmake openssl vscode libreoffice-fresh postman discord octave pyenv-win vagrant nextcloud-client lein razer-synapse-3 forticlientvpn wsl-ubuntu-2004
+choco upgrade -y vlc cmake openssl vscode libreoffice-fresh postman discord octave pyenv-win vagrant nextcloud-client lein razer-synapse-3 forticlientvpn wsl-ubuntu-2204 powertoys
 choco install -y mysql --version=5.7.18
-choco install -y anaconda3 --params /AddToPath
+choco install -y anaconda3 --params '"/JustMe /AddToPath"'
+
+irm get.scoop.sh | iex
+
+scoop install neovim
+
+Install-PackageProvider -Name NuGet -Confirm:$False
+Install-Module -Name 'posh-git' -Scope 'CurrentUser' -Confirm:$False -Force
+del $PROFILE
+New-Item -Type File -Path $PROFILE -Force
+Add-Content -Path $PROFILE -value "Import-Module 'posh-git'"
 
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
@@ -19,5 +29,7 @@ if(!(Test-Path ~/tridactyl.xpi)) {
   Start-Process firefox ~/tridactyl.xpi
 }
 
-Read-Host 'Press Enter to continue…' | Out-Null
+Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted
+
+Read-Host 'Press Enter to continueï¿½' | Out-Null
 
