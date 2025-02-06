103b366d5d6ab5112638c3f04bdc1b426005425f
Sergio Alvarez-Napagao <sergio@Sergios-MacBook-Pro.local>
2025-02-06 22:49:51 +0100

    Update to Sequoia

--- bootstrap-macos.sh
+++ bootstrap-macos.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 
-touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
-PROD=$(softwareupdate -l |
-  grep "\*.*Command Line" |
-  head -n 1 | awk -F"*" '{print $2}' |
-  sed -e 's/^ *//' |
-  tr -d '\n')
-softwareupdate -i "$PROD" --verbose;
-sudo softwareupdate --install-rosetta
-
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
