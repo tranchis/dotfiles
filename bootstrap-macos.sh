ed9c83e5134371da353096bd431e9f17bf00bba1
Sergio Alvarez-Napagao <sergio@iClouds-Mac-mini.local>
2025-04-10 03:07:51 +0200

    Adapt to Sononma

--- bootstrap-macos.sh
+++ bootstrap-macos.sh
@@ -1,11 +1,32 @@
 #!/bin/sh
 
+processorBrand=$(/usr/sbin/sysctl -n machdep.cpu.brand_string)
+if [[ "${processorBrand}" = *"Apple"* ]]; then
+  echo "Apple Processor is present."
+  checkRosettaStatus=$(/bin/launchctl list | /usr/bin/grep "com.apple.oahd-root-helper")
+  RosettaFolder="/Library/Apple/usr/share/rosetta"
+  if [[ -e "${RosettaFolder}" && "${checkRosettaStatus}" != "" ]]; then
+    echo "Rosetta Folder exists and Rosetta Service is running. Exiting..."
+  else
+    echo "Rosetta Folder does not exist or Rosetta service is not running. Installing Rosetta..."
+    /usr/sbin/softwareupdate --install-rosetta --agree-to-license
+    if [[ $? -eq 0 ]]; then
+      echo "Rosetta installed successfully."
+    else
+      echo "Rosetta installation failed."
+    fi
+  fi
+else
+  echo "Apple Processor is not present. Rosetta not required."
+fi
+
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 pip3 install ansible
 brew install ansible
 
+mkdir ~/Applications
 mkdir -p ~/repos/
 cd ~/repos
 git clone https://github.com/tranchis/dotfiles.git
--- roles/vim/templates/wezterm.lua
+++ roles/vim/templates/wezterm.lua
@@ -11,26 +11,10 @@ local config = wezterm.config_builder()
 -- config.color_scheme = "Desert"
 -- config.color_scheme = "DimmedMonokai"
 -- config.color_scheme = "zenwritten_dark"
--- config.color_scheme = "Fairy Floss (Gogh)"
--- config.color_scheme = "Fairy Floss Dark (Gogh)"
--- config.color_scheme = "Palenight (Gogh)"
--- config.color_scheme = "Ocean (dark) (terminal.sexy)"
--- config.color_scheme = "Ocean (light) (terminal.sexy)"
--- config.color_scheme = "Ocean Dark (Gogh)"
--- config.color_scheme = "Ivory Dark (terminal.sexy)"
--- config.color_scheme = "iceberg-dark"
--- config.color_scheme = "tokyonight"
--- config.color_scheme = "Tokyo Night Storm (Gogh)"
-config.color_scheme = "Tokyo Night Storm"
--- config.color_scheme = "tokyonight_moon"
--- config.color_scheme = "tokyonight_night"
--- config.color_scheme = "Tomorrow (dark) (terminal.sexy)"
+config.color_scheme = "Palenight (Gogh)"
 
 -- config.font = wezterm.font("JetBrains Mono", { weight = "Bold", italic = true })
 config.font = wezterm.font("JetBrains Mono")
--- config.font = wezterm.font("Fira Code Retina")
--- config.font_size = 16.0
--- config.font_size = 15.0
 config.font_size = 14.0
 
 local act = wezterm.action
@@ -51,6 +35,8 @@ config.keys = {
 		action = act.SendKey({ key = "f", mods = "ALT" }),
 	},
 }
+
+config.window_close_confirmation = "NeverPrompt"
 
 -- and finally, return the configuration to wezterm
 return config
