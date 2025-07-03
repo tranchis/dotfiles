9dcfb74b6f7115735a2281dc2ce7e23fc14ceacc
Sergio Alvarez-Napagao <tranchis@gmail.com>
2025-07-03 13:14:48 +0200

    Adapt to newer versions of Ubuntu 22.04

--- bootstrap-ubuntu2204.sh
+++ bootstrap-ubuntu2204.sh
@@ -4,13 +4,14 @@
 
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install -y ansible
 
 bash <(curl -s https://gist.githubusercontent.com/alokyadav15/c3a2bbe6089ceff286215113bd092703/raw/3a3dd9af2ec59e4756bee5282e7c1e714dbf7db2/setup_fonts.sh)
 
 test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
 test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 eval $($(brew --prefix)/bin/brew shellenv)
+
+brew install ansible
 
 ansible-galaxy collection install community.general
 ansible-playbook -K -i inventory ubuntu-2204.yml
